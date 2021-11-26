e2d905f25fde96452c71e94eef3c640dc2f874c3
Oliver Burt <oliburt@gmail.com>
2021-11-26 17:08:01 +0000

    basic setup for fetchCourier

--- src/controllers/couriers.ts
+++ src/controllers/couriers.ts
@@ -1,6 +1,15 @@
 import { Request, Response } from "express";
+import Courier from "../models/Courier";
 
 export async function fetchCourier(req: Request, res: Response) {
-  const dummy = { id: 123, capacity: 10 };
-  return res.json(dummy);
+  try {
+    const { id } = req.params;
+    const courier = await Courier.findByPk(id);
+    if (courier == null) {
+      res.send(null);
+    }
+    return res.json(courier);
+  } catch (error) {
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 }
--- src/tests/couriers.test.ts
+++ src/tests/couriers.test.ts
@@ -1,23 +1,36 @@
 import supertest, { SuperTest, Test } from "supertest";
-
+import Courier from "../models/Courier";
+import sequelize from "../data";
 import app from "../app";
 
 describe("Couriers", () => {
   let testApp: SuperTest<Test>;
 
   beforeAll(async () => {
+    await sequelize.sync();
     testApp = supertest(app);
+  });
+
+  afterAll(async () => {
+    await sequelize.close();
   });
 
   describe("GET /couriers/:id", () => {
     test("works", async () => {
+      const courier = await Courier.create({
+        id: 123,
+        max_capacity: 10
+      });
       const response = await testApp
         .get("/couriers/123")
         .set("Accept", "application/json")
         .expect(200)
         .expect("Content-Type", /json/);
 
-      expect(response.body).toEqual({ id: 123, capacity: 10 });
+      expect(response.body).toMatchObject({
+        id: courier.get("id"),
+        max_capacity: courier.get("max_capacity")
+      });
     });
   });
 });
