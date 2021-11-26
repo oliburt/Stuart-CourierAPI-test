4be3dedd14fb33eb17b94c7e00fe99f48f90d60a
Oliver Burt <oliburt@gmail.com>
2021-11-26 18:50:03 +0000

    add build with Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+## BUILD APP
+FROM node:16-alpine AS builder
+WORKDIR /app
+
+COPY package.json ./
+COPY tsconfig.json ./
+COPY src ./src
+
+RUN npm install
+RUN npm run build
+
+
+## CONTAINERIZE BUILT APP
+FROM node:current-alpine
+WORKDIR /app
+
+COPY package.json ./
+RUN npm install --production
+COPY --from=builder /app/build .
+
+EXPOSE 3000
+CMD ["node", "server.js"]
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,15 @@
+import process from "process";
 import app from "./app";
 import config from "./config";
+import sequelize from "./data/index";
 
-app.listen(config.PORT, () => {
-  console.log("Server listening on port:", config.PORT);
+// Exit process on Ctrl-C in Docker container
+process.on("SIGINT", () => {
+  process.exit(0);
+});
+
+sequelize.sync().then(() => {
+  app.listen(config.PORT, () => {
+    console.log("Server listening on port:", config.PORT);
+  });
 });
