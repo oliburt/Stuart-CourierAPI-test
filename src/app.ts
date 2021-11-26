39a3ace8ac8d4ac7700158f3732e4ea3d2bd7cc9
Oliver Burt <oliburt@gmail.com>
2021-11-26 16:44:43 +0000

    catch all 404

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import registerRoutes from "./routes/";
 
 const app = express();
@@ -6,5 +6,12 @@ const app = express();
 app.use(express.json());
 
 registerRoutes(app);
+
+// Catch all 404 Route Not Found
+app.use((_req: Request, res: Response) => {
+  console.log(_req.method);
+
+  res.status(404).json({ message: "Route Not Found" });
+});
 
 export default app;
