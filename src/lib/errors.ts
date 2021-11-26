65240fd06bf068d9b7f61e73ecfed0850dc37a8f
Oliver Burt <oliburt@gmail.com>
2021-11-26 17:29:26 +0000

    add handler for internal server error

--- src/lib/errors.ts
+++ src/lib/errors.ts
@@ -1,19 +1,37 @@
 import { Response } from "express";
 
 export const Messages = {
-  CourierNotFound: "Courier Not Found"
+  CourierNotFound: "Courier Not Found",
+  InternalServerError: "Internal Server Error"
 };
 
 export interface CustomError extends Error {
   send(res: Response): void;
 }
 
-export class CourierNotFound extends Error {
+export function isCustomError(e: unknown): e is CustomError {
+  if (e instanceof Error) {
+    return "send" in e;
+  }
+  return false;
+}
+
+export class CourierNotFound extends Error implements CustomError {
   constructor() {
     super(Messages.CourierNotFound);
   }
 
   send(res: Response) {
     return res.status(404).json({ message: this.message });
+  }
+}
+
+export class InternalServerError extends Error implements CustomError {
+  constructor() {
+    super(Messages.InternalServerError);
+  }
+
+  send(res: Response) {
+    return res.status(500).json({ message: this.message });
   }
 }
--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,7 +1,12 @@
 import { Response } from "express";
+import { InternalServerError, isCustomError } from "../lib/errors";
 import logger from "./logger";
 
-export function handleErrorResponse(error: any, response: Response) {
+export function handleErrorResponse(error: unknown, response: Response) {
   if (error != null) logger.error(error);
-  return error?.send(response);
+  if (isCustomError(error)) {
+    return error.send(response);
+  }
+
+  return new InternalServerError().send(response);
 }
